770eb12ff7e7a75bcd6e2573a5ab64e3aa3c98c7
zhenghuiyang00 <2592142500@qq.com>
2019-12-02 19:06:20 +0800

    p

---
+++ app/src/main/java/combwei/one_wek1/MainActivity.java
@@ -0,0 +1,15 @@
+package combwei.one_wek1;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        System.out.println("郑惠洋！！！丁鸿翔");
+    }
+}
